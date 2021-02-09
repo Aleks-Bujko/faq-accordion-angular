2fa02cc0ffe4a94310d6963672657cc563fcf47c
Aleks Bujko <aleksander.bujko@unterschiedundmacher.com>
2021-02-09 15:36:30 +0100

    created basic structure and dummy data

--- faq-accordion/src/app/app.module.ts
+++ faq-accordion/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SingleQuestionComponent } from './single-question/single-question.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SingleQuestionComponent
   ],
   imports: [
     BrowserModule
